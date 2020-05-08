593292408b870204d01beeb61c86e7a9c7b1a6dd
mlugg <mlugg@mlugg.co.uk>
2020-05-08 02:19:31 +0100

    Add basic output module for pretty-printing types

---
+++ src/QntSyn/Output.hs
@@ -0,0 +1,20 @@
+module QntSyn.Output where
+
+import QntSyn
+import Text.Printf
+
+isOperator :: String -> Bool
+isOperator "" = False
+isOperator x = head x `elem` "!#$%&*+./<=>?@\\^|-~:"
+
+pPrintType :: Type -> String
+
+pPrintType (TApplication (TApplication (TConcrete x) y) z)
+  | isOperator x = printf "(%s %s %s)" (pPrintType y) x (pPrintType z)
+  | otherwise    = printf "((%s %s) %s)" x (pPrintType y) (pPrintType z)
+
+pPrintType (TApplication x y) = printf "(%s %s)" (pPrintType x) (pPrintType y)
+
+pPrintType (TConcrete x) = x
+
+pPrintType (TVariable i) = 'a' : show i
